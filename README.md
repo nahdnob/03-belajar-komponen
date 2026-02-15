1ea643851a17e3ebcd3e35a812a95cd6183f2982
nahdnob <nahdnob@gmail.com>
2026-02-15 11:13:05 +0700

    W03: Jawaban soal 3

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Berikut adalah hasil implementasi komponen Profile pada halaman utama:
 
 ![Hasil Tampilan](./ss-soal-1.png)
 ![Hasil Tampilan](./ss-soal-2.png)
+![Hasil Tampilan](./ss-soal-3.png)
 
 ## Apa yang Saya Pelajari
 Pada tugas ini saya mempelajari:
@@ -17,6 +18,10 @@ Pada tugas ini saya mempelajari:
     Soal 2:
         - Cara membuat Named Export Components {Gallery}
         - Cara menggunakan komponen Gallery pada Page.tsx
+    Soal 3:
+        - JSX hanya boleh me-return satu elemen utama (root)
+        - Atribut HTML `class` harus ditulis sebagai `className` (camelCase)
+        - Tag HTML harus ditutup dengan benar seperti `<br>` harus menggunakan `<br />`
 
 ## Error yang Ditemui dan Solusinya
 Soal 1: Saya sempat mengalami error saat menampilkan gambar dari URL eksternal.
@@ -28,8 +33,13 @@ Solusi:
   pada file `next.config.ts`
 - Menyesuaikan hostname sesuai domain gambar (i.imgur.com)
 
-Setelah konfigurasi ditambahkan dan server di-restart,
-gambar dapat ditampilkan dengan normal.
+Soal 3: Error terjadi karena JSX memiliki aturan yang lebih ketat
+dibandingkan HTML biasa, sehingga penulisan yang tidak sesuai
+akan menyebabkan error saat kompilasi.
+
+Solusi:
+Solusi dilakukan dengan membungkus elemen ke dalam satu parent,
+memperbaiki atribut JSX, dan merapikan penulisan tag HTML.
 
 ## Kesimpulan
 Dengan tugas ini, saya memahami konsep dasar komponen di Next.js
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,49 @@
+// ------------------------- Soal 1 -------------------------
 // import Profile from "@/src/components/profile";
-import { Gallery } from "@/src/components/gallery";
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1 className="mt-5 text-center text-2xl font-bold">My Profile</h1>
+//       <br />
+//       <h1 className="text-center text-2xl font-normal">
+//         Ilmuwan yang luar biasa
+//       </h1>
+//       <div className="flex justify-center gap-6 mt-6">
+//         <profile />
+//         <profile />
+//         <profile />
+//       </div>
+//     </main>
+//   );
+// }
+
+// ------------------------- Soal 2 -------------------------
+// import { Gallery } from "@/src/components/gallery";
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1 className="mt-5 text-center text-2xl font-bold">My Profile</h1>
+//       <br />
+//       <h1 className="text-center text-2xl font-normal">Ilmuwan yang luar biasa</h1>
+//       <div className="flex justify-center gap-6 mt-6">
+//         <Gallery />
+//         <Gallery />
+//         <Gallery />
+//       </div>
+//     </main>
+//   );
+// }
+
+// ------------------------- Soal 3 -------------------------
+import Bio from "@/src/components/bio";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="mt-5 text-center text-2xl font-bold">My Profile</h1>
-      <br />
-      <h1 className="text-center text-2xl font-normal">Ilmuwan yang luar biasa</h1>
       <div className="flex justify-center gap-6 mt-6">
-        <Gallery />
-        <Gallery />
-        <Gallery />
+        <Bio />
       </div>
     </main>
   );
